ba199fa650c6ddba60005d8f8c44d794bf425eaf
Excale <dev_excale@hotmail.com>
2025-09-16 22:51:47 +0200

    New config screen
    Signed-off-by: Excale <dev_excale@hotmail.com>

--- src/aqthooks/aqt_gui_config.py
+++ src/aqthooks/aqt_gui_config.py
@@ -3,10 +3,12 @@ from typing import Optional
 from aqt import mw
 from aqt.qt import (
 	Qt, QDialog, QVBoxLayout, QHBoxLayout, QFormLayout,
-	QLabel, QLineEdit, QPushButton, QCheckBox, QWidget
+	QLabel, QLineEdit, QPushButton, QCheckBox, QWidget,
+	QListWidget, QStackedWidget
 )
 
 from .qt_utils import hover_label, input_color_preview
+from ..__version__ import VERSION
 from ..addon_config import AddonConfig
 
 
@@ -17,58 +19,12 @@ class AddonConfigPane(QDialog):
 
 	def __init__(self, parent: Optional[QWidget] = None) -> None:
 		super().__init__(parent=parent)
+		self.setWindowTitle("Settings/Help - Sky's Jouzu BulkOps")
+		self.setMinimumSize(450, 400)
 
-		self.setWindowTitle('Sky\'s Jouzu BulkOps - Settings')
-
-		# Layouts
-		self.main_layout: Optional[QVBoxLayout] = None
-		self.unpack_form_layout: Optional[QFormLayout] = None
-		self.pitch_form_layout: Optional[QFormLayout] = None
-		self.buttons_layout: Optional[QHBoxLayout] = None
-
-		# Main Title
-		self.lbl_title: Optional[QLabel] = None
-		self.lbl_help: Optional[QLabel] = None
-
-		# --- [Unpack] Section ---
-		self.lbl_unpack_subtitle: Optional[QLabel] = None
-
-		# Labels for Unpack config
-		self.lbl_unpack_dictionary_field: Optional[QLabel] = None
-		self.lbl_unpack_reading_field: Optional[QLabel] = None
-		self.lbl_unpack_tag_fail: Optional[QLabel] = None
-
-		# Inputs for Unpack config
-		self.input_unpack_dictionary_field: Optional[QLineEdit] = None
-		self.input_unpack_reading_field: Optional[QLineEdit] = None
-		self.input_unpack_tag_fail: Optional[QLineEdit] = None
-
-		# --- [Pitch] Section ---
-		self.lbl_pitch_subtitle: Optional[QLabel] = None
-
-		# Labels for Pitch config
-		self.lbl_pitch_reading_field: Optional[QLabel] = None
-		self.lbl_pitch_tocolour_fields: Optional[QLabel] = None
-		self.lbl_pitch_heiban_colour: Optional[QLabel] = None
-		self.lbl_pitch_atamadaka_colour: Optional[QLabel] = None
-		self.lbl_pitch_nakadaka_colour: Optional[QLabel] = None
-		self.lbl_pitch_oodaka_colour: Optional[QLabel] = None
-		self.lbl_pitch_colour_graph: Optional[QLabel] = None
-		self.lbl_pitch_tag_fail: Optional[QLabel] = None
-
-		# Inputs for Pitch config
-		self.input_pitch_reading_field: Optional[QLineEdit] = None
-		self.input_pitch_tocolour_fields: Optional[QLineEdit] = None
-		self.input_pitch_heiban_colour: Optional[QLineEdit] = None
-		self.input_pitch_atamadaka_colour: Optional[QLineEdit] = None
-		self.input_pitch_nakadaka_colour: Optional[QLineEdit] = None
-		self.input_pitch_oodaka_colour: Optional[QLineEdit] = None
-		self.chk_pitch_colour_graph: Optional[QCheckBox] = None
-		self.input_pitch_tag_fail: Optional[QLineEdit] = None
-
-		# Dialog buttons
-		self.btn_save: Optional[QPushButton] = None
-		self.btn_cancel: Optional[QPushButton] = None
+		# Main layout widgets
+		self.list_widget: Optional[QListWidget] = None
+		self.stacked_widget: Optional[QStackedWidget] = None
 
 		# Set the main layout for the dialog
 		self.setLayout(self.__layout__())
@@ -76,83 +32,134 @@ class AddonConfigPane(QDialog):
 		# Load existing configuration
 		self.__load_configuration__()
 
-		# Adjust dialog size to fit contents
-		self.adjustSize()
-
-		return
+		# Select the first item by default
+		if self.list_widget.count() > 0:
+			self.list_widget.setCurrentRow(0)
 
 	def __layout__(self) -> QVBoxLayout:
 		"""
-		Create and return the layout for the whole dialog.
+		Create and return the main layout for the dialog.
+		This consists of a vertical layout containing the two-pane horizontal layout
+		and the dialog buttons at the bottom.
+		"""
+		# Top-level layout
+		main_layout = QVBoxLayout(self)
+
+		# Two-pane horizontal layout for settings
+		paned_layout = QHBoxLayout()
+
+		# Left pane: Category list
+		self.list_widget = QListWidget()
+		self.list_widget.setFixedWidth(150)
+
+		# Right pane: Stack of configuration pages
+		self.stacked_widget = QStackedWidget()
+
+		# Create and add pages to the list and stack
+		self.list_widget.addItem("About")
+		self.stacked_widget.addWidget(self.__create_page_about__())
+		self.list_widget.addItem("Unpack Dictionary")
+		self.stacked_widget.addWidget(self.__create_page_unpack__())
+		self.list_widget.addItem("Pitch Accent")
+		self.stacked_widget.addWidget(self.__create_page_pitch__())
+
+		# Connect list widget to stacked widget
+		self.list_widget.currentRowChanged.connect(self.stacked_widget.setCurrentIndex)
+
+		# Add panes to the settings layout
+		paned_layout.addWidget(self.list_widget)
+		paned_layout.addWidget(self.stacked_widget)
+
+		# Add the main settings layout and the buttons
+		main_layout.addLayout(paned_layout)
+		main_layout.addLayout(self.__layout_dialog_buttons__())
+
+		return main_layout
+
+	@staticmethod
+	def __create_page_about__() -> QWidget:
+		"""
+		Create and return the widget+layout for the About section.
 		"""
 
-		self.main_layout = QVBoxLayout(self)
+		widget_about = QWidget()
+		layout_about = QVBoxLayout(widget_about)
 
 		# Title label
-		self.lbl_title = QLabel('Configuration')
-		self.lbl_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
-		font_title = self.lbl_title.font()
-		font_title.setPointSize(16)
-		font_title.setBold(True)
-		self.lbl_title.setFont(font_title)
+		lbl_title = QLabel(f"Sky's Jouzu BulkOps")
+		lbl_title.setAlignment(Qt.AlignmentFlag.AlignLeft)
+		lbl_title.setStyleSheet(
+			'font-size: 16pt;'
+			'font-weight: bold;'
+			'margin-bottom: 0;'
+		)
+
+		# Subtitle version
+		lbl_version = QLabel(f'v{VERSION}')
+		lbl_version.setAlignment(Qt.AlignmentFlag.AlignLeft)
+		lbl_version.setStyleSheet(
+			'color: gray;'
+			'font-size: 10pt;'
+			'font-style: italic;'
+			'margin-top: 0;'
+		)
 
 		# Help label
-		self.lbl_help = QLabel(
-			'Welcome to the Addon Configuration!\n'
-			'This interface can help you setup the functionalities this addon offers.\n\n'
+		lbl_help = QLabel(
+			"Welcome to Sky's Jouzu BulkOps' configuration screen!\n\n"
+			'This interface can help you setup the functionalities the addon offers. '
+			"You can use the left pane to navigate between each functionality's settings.\n\n"
 			'To find out what each option does, you can hover over the labels '
 			'and a tooltip will appear with a description.'
 		)
+		lbl_help.setWordWrap(True)
 
-		# Build layout
-		self.main_layout.addWidget(self.lbl_title)
-		self.main_layout.addSpacing(10)
-		self.main_layout.addWidget(self.lbl_help)
-		self.main_layout.addSpacing(10)
-		self.main_layout.addLayout(self.__layout_unpack__())
-		self.main_layout.addSpacing(15)
-		self.main_layout.addLayout(self.__layout_pitch__())
-		self.main_layout.addStretch(1)  # Push buttons to the bottom
-		self.main_layout.addLayout(self.__layout_dialog_buttons__())
+		layout_about.addWidget(lbl_title)
+		layout_about.addWidget(lbl_version)
+		layout_about.addSpacing(10)
+		layout_about.addWidget(lbl_help)
+		layout_about.addStretch()  # Push content to the top
 
-		return self.main_layout
+		return widget_about
 
-	def __layout_unpack__(self) -> QVBoxLayout:
+	def __create_page_unpack__(self) -> QWidget:
 		"""
-		Create and return the layout for the Unpack section.
+		Create and return the widget+layout for the Unpack section.
 		"""
 
-		layout_unpack = QVBoxLayout()
+		widget_unpack = QWidget()
+		layout_unpack = QVBoxLayout(widget_unpack)
+		layout_unpack.setContentsMargins(10, 10, 10, 10)
 
 		# Subtitle
-		self.lbl_unpack_subtitle = QLabel('-- Unpack --')
-		font_subtitle = self.lbl_unpack_subtitle.font()
-		font_subtitle.setBold(True)
-		font_subtitle.setPointSize(12)
-		self.lbl_unpack_subtitle.setFont(font_subtitle)
+		lbl_unpack_subtitle = QLabel('Settings / Dictionary Unpack')
+		lbl_unpack_subtitle.setStyleSheet(
+			'font-size: 14pt;'
+			'font-weight: bold;'
+		)
 
 		# Form layout for inputs
 		self.unpack_form_layout = QFormLayout()
-		self.unpack_form_layout.setContentsMargins(20, 5, 0, 5)
+		self.unpack_form_layout.setContentsMargins(0, 15, 0, 5)
 
 		# 'Dictionary field' input
 		self.lbl_unpack_dictionary_field = hover_label(
 			'Dictionary field:',
-			'Name of the field where the dictionary output is'
+			'Name of the field where the dictionary output is',
 		)
 		self.input_unpack_dictionary_field = QLineEdit()
 
 		# 'Reading field' input
 		self.lbl_unpack_reading_field = hover_label(
 			'Reading field:',
-			'Name of the field where the kana reading should go'
+			'Name of the field where the kana reading should go',
 		)
 		self.input_unpack_reading_field = QLineEdit()
 
 		# 'Tag fail' input
 		self.lbl_unpack_tag_fail = hover_label(
 			'Tag fail:',
-			'Name of the tag to apply when the operation fails, leave blank for none'
+			'Name of the tag to apply when the operation fails, leave blank for none',
 		)
 		self.input_unpack_tag_fail = QLineEdit()
 
@@ -162,61 +169,64 @@ class AddonConfigPane(QDialog):
 		self.unpack_form_layout.addRow(self.lbl_unpack_tag_fail, self.input_unpack_tag_fail)
 
 		# Build layout
-		layout_unpack.addWidget(self.lbl_unpack_subtitle)
+		layout_unpack.addWidget(lbl_unpack_subtitle)
 		layout_unpack.addLayout(self.unpack_form_layout)
+		layout_unpack.addStretch(1)
 
-		return layout_unpack
+		return widget_unpack
 
-	def __layout_pitch__(self) -> QVBoxLayout:
+	def __create_page_pitch__(self) -> QWidget:
 		"""
-		Create and return the layout for the Pitch section.
+		Create and return the widget+layout for the Pitch section.
 		"""
 
-		layout_pitch = QVBoxLayout()
+		widget_pitch = QWidget()
+		layout_pitch = QVBoxLayout(widget_pitch)
+		layout_pitch.setContentsMargins(10, 10, 10, 10)
 
 		# Subtitle
-		self.lbl_pitch_subtitle = QLabel('-- Pitch --')
-		font_subtitle = self.lbl_pitch_subtitle.font()
-		font_subtitle.setBold(True)
-		font_subtitle.setPointSize(12)
-		self.lbl_pitch_subtitle.setFont(font_subtitle)
+		lbl_pitch_subtitle = QLabel('Settings / Pitch Accent')
+		lbl_pitch_subtitle.setStyleSheet(
+			'font-size: 14pt;'
+			'font-weight: bold;'
+		)
 
 		# Form layout for inputs
 		self.pitch_form_layout = QFormLayout()
-		self.pitch_form_layout.setContentsMargins(20, 5, 0, 5)
+		self.pitch_form_layout.setContentsMargins(0, 15, 0, 5)
 
 		# 'Reading field' input
 		self.lbl_pitch_reading_field = hover_label(
 			'Reading field:',
-			'Name of the field where the kana reading and the pitch graph are'
+			'Name of the field where the kana reading and the pitch graph are',
 		)
 		self.input_pitch_reading_field = QLineEdit()
 
 		# 'Fields to colour' input
 		self.lbl_pitch_tocolour_fields = hover_label(
 			'Fields to colour:',
-			'Comma-separated list of fields to apply the pitch colours to'
+			'Comma-separated list of fields to apply the pitch colours to',
 		)
 		self.input_pitch_tocolour_fields = QLineEdit()
 
 		# Heiban colour input
 		self.lbl_pitch_heiban_colour = hover_label(
 			'Heiban colour:',
-			'Colour for Heiban pitch type<br>e.g., #RRGGBB or a valid color name'
+			'Colour for Heiban pitch type<br>e.g., #RRGGBB or a valid color name',
 		)
 		input_color_preview_heiban, self.input_pitch_heiban_colour, _ = input_color_preview('#RRGGBB or name')
 
 		# Atamadaka colour input
 		self.lbl_pitch_atamadaka_colour = hover_label(
 			'Atamadaka colour:',
-			'Colour for Atamadaka pitch type<br>e.g., #RRGGBB or a valid color name'
+			'Colour for Atamadaka pitch type<br>e.g., #RRGGBB or a valid color name',
 		)
 		input_color_preview_atamadaka, self.input_pitch_atamadaka_colour, _ = input_color_preview('#RRGGBB or name')
 
 		# Nakadaka colour input
 		self.lbl_pitch_nakadaka_colour = hover_label(
 			'Nakadaka colour:',
-			'Colour for Nakadaka pitch type<br>e.g., #RRGGBB or a valid color name'
+			'Colour for Nakadaka pitch type<br>e.g., #RRGGBB or a valid color name',
 		)
 		input_color_preview_nakadaka, self.input_pitch_nakadaka_colour, _ = input_color_preview('#RRGGBB or name')
 
@@ -230,14 +240,14 @@ class AddonConfigPane(QDialog):
 		# 'Colour graph' checkbox
 		self.lbl_pitch_colour_graph = hover_label(
 			'Colour graph:',
-			'Whether to apply the colour to the pitch graph as well'
+			'Whether to apply the colour to the pitch graph as well',
 		)
 		self.chk_pitch_colour_graph = QCheckBox()
 
 		# 'Tag fail' input
 		self.lbl_pitch_tag_fail = hover_label(
 			'Tag fail:',
-			'Name of the tag to apply when the operation fails,<br>leave blank for none'
+			'Name of the tag to apply when the operation fails,<br>leave blank for none',
 		)
 		self.input_pitch_tag_fail = QLineEdit()
 
@@ -252,11 +262,11 @@ class AddonConfigPane(QDialog):
 		self.pitch_form_layout.addRow(self.lbl_pitch_tag_fail, self.input_pitch_tag_fail)
 
 		# Build layout
-		layout_pitch.addWidget(self.lbl_pitch_subtitle)
+		layout_pitch.addWidget(lbl_pitch_subtitle)
 		layout_pitch.addLayout(self.pitch_form_layout)
-		return layout_pitch
+		layout_pitch.addStretch(1)
 
-	# Placeholder: User might want more sophisticated validation or error handling.
+		return widget_pitch
 
 	def __layout_dialog_buttons__(self) -> QHBoxLayout:
 		"""
@@ -265,7 +275,13 @@ class AddonConfigPane(QDialog):
 		:return: A layout containing the dialog's buttons
 		"""
 
-		self.buttons_layout = QHBoxLayout()
+		buttons_layout = QHBoxLayout()
+
+		lbl_savehint = QLabel('Remember to click Save to apply changes!')
+		lbl_savehint.setStyleSheet(
+			'color: gray;'
+			'font-style: italic;'
+		)
 
 		# Save button
 		self.btn_save = QPushButton('Save')
@@ -276,11 +292,12 @@ class AddonConfigPane(QDialog):
 		self.btn_cancel.clicked.connect(self.reject)
 
 		# Build layout
-		self.buttons_layout.addStretch(1)  # Push buttons to the right
-		self.buttons_layout.addWidget(self.btn_save)
-		self.buttons_layout.addWidget(self.btn_cancel)
+		buttons_layout.addWidget(lbl_savehint)
+		buttons_layout.addStretch(1)  # Push buttons to the right
+		buttons_layout.addWidget(self.btn_save)
+		buttons_layout.addWidget(self.btn_cancel)
 
-		return self.buttons_layout
+		return buttons_layout
 
 	def __load_configuration__(self) -> None:
 		"""
@@ -322,7 +339,9 @@ class AddonConfigPane(QDialog):
 
 		# Pitch section
 		conf.pitch.field_reading = self.input_pitch_reading_field.text().strip()
-		conf.pitch.fields_tocolour = list(map(str.strip, self.input_pitch_tocolour_fields.text().split(',')))
+		conf.pitch.fields_tocolour = [
+			field.strip() for field in self.input_pitch_tocolour_fields.text().split(',') if field.strip()
+		]
 		conf.pitch.colour_heiban = self.input_pitch_heiban_colour.text().strip()
 		conf.pitch.colour_atamadaka = self.input_pitch_atamadaka_colour.text().strip()
 		conf.pitch.colour_nakadaka = self.input_pitch_nakadaka_colour.text().strip()
